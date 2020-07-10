470472705217b064abe7414c137d2c32c5bcfee3
lorenkgifford <lorenkgifford@yahoo.com>
2020-07-10 14:51:26 -0400

    1) Add User ViewModel, Controller and View. 2) Add Fibonacci Sequence Generator and logging.

---
+++ Controllers/FibonacciController.cs
@@ -0,0 +1,53 @@
+﻿using NLog;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Web.Mvc;
+
+namespace TestWebApp.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        Logger log = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            List<string> FibonacciStringList = new List<string>();
+            List<BigInteger> FibonacciList = GenerateFibonacciSequenceList(50);
+
+            foreach (BigInteger item in FibonacciList)
+            {
+                FibonacciStringList.Add(item.ToString());
+            }
+            return View(FibonacciStringList);
+        }
+
+        private List<BigInteger> GenerateFibonacciSequenceList(int NumberOfTerms)
+        {
+            List<BigInteger> FibonacciList = new List<BigInteger>();
+            BigInteger t1 = 0, t2 = 1, nextTerm;
+
+            for (int i = 1; i <= NumberOfTerms; ++i)
+            {
+                if (i == 1)
+                {
+                    nextTerm = t1;
+                }
+                else if (i == 2)
+                {
+                    nextTerm = t2;
+                }
+                else
+                {
+                    nextTerm = t1 + t2;
+                    t1 = t2;
+                    t2 = nextTerm;
+                }
+
+                FibonacciList.Add(nextTerm);
+
+                log.Debug("term: {0}, value: {1}", i, nextTerm);
+            }
+            return FibonacciList;
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApp.ViewModels;
+
+namespace TestWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            List<UserVM> UserVMlist = new List<UserVM>();
+            foreach (Data.User item in Data.DataRepository.GetAllUsers())
+            {
+                UserVM VMItem = new UserVM();
+                VMItem.Id = item.Id;
+                VMItem.Name = item.Name;
+                VMItem.DateModified = item.DateModified;
+                VMItem.IsActive = item.IsActive;
+                UserVMlist.Add(VMItem);
+            }
+            return View(UserVMlist);
+        }
+
+        public ActionResult Create(UserVM newUser)
+        {
+            if (!String.IsNullOrEmpty(newUser.Name))
+            {
+                Data.User user = new Data.User();
+                user.Id = newUser.Id;
+                user.Name = newUser.Name;
+                user.DateModified = newUser.DateModified;
+                user.IsActive = newUser.IsActive;
+                Data.DataRepository.AddUser(user);
+                return RedirectToAction("Index");
+            }
+            return View(newUser);
+        }
+
+        public ActionResult Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
